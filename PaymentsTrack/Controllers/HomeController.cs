f6a8053295b9a8f88a1087bd6061ad14e3f31e21
unknown <maik.glez.lugo@gmail.com>
2020-01-27 22:18:09 -0600

    Coding challenge

---
+++ PaymentsTrack/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaymentsTrack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private PaymentTrackerEntities db = new PaymentTrackerEntities();
+        public ActionResult Index()
+        {
+            ViewBag.TotalPayments = db.Payments.Sum(p => p.Amount);
+            ViewBag.TotalPaymentsCurrentMonth = db.Payments.Where(p=>p.CreatedOn.Month == DateTime.Now.Month).Sum(p => p.Amount);
+            var lastSevenDays = DateTime.Now.AddDays(-7);
+            ViewBag.TotalPaymentsLastSevenDays = db.Payments.Where(p => p.CreatedOn >= lastSevenDays).Sum(p => p.Amount);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PaymentsTrack/Controllers/PaymentController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using PaymentsTrack;
+
+namespace PaymentsTrack.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private PaymentTrackerEntities db = new PaymentTrackerEntities();
+
+        // GET: Payment
+        public ActionResult Index()
+        {
+            var payments = db.Payments.Include(p => p.Category);
+            return View(payments.ToList());
+        }
+
+        // GET: Payment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payments payments = db.Payments.Find(id);
+            if (payments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payments);
+        }
+
+        // GET: Payment/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name");
+            return View();
+        }
+
+        // POST: Payment/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CategoryId,Description,Amount,CreatedOn,UserId")] Payments payments)
+        {
+            if (ModelState.IsValid)
+            {
+                payments.CreatedOn = DateTime.Now;
+                payments.UserId = HttpContext.User.Identity.GetUserId();
+                db.Payments.Add(payments);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name", payments.CategoryId);
+            return View(payments);
+        }
+
+        // GET: Payment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payments payments = db.Payments.Find(id);
+            if (payments == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name", payments.CategoryId);
+            return View(payments);
+        }
+
+        // POST: Payment/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CategoryId,Description,Amount,CreatedOn,UserId")] Payments payments)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payments).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Category, "Id", "Name", payments.CategoryId);
+            return View(payments);
+        }
+
+        // GET: Payment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payments payments = db.Payments.Find(id);
+            if (payments == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payments);
+        }
+
+        // POST: Payment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Payments payments = db.Payments.Find(id);
+            db.Payments.Remove(payments);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
